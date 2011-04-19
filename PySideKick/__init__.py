79667abbb1ed9a5e850d6685e5375a1960490c51
Ryan Kelly <ryan@rfk.id.au>
2011-04-20 09:27:29 +1000

    bump for release

--- PySideKick/__init__.py
+++ PySideKick/__init__.py
@@ -22,7 +22,7 @@ widgets for building applications with PySide.  So far we have:
 
 __ver_major__ = 0
 __ver_minor__ = 2
-__ver_patch__ = 2
+__ver_patch__ = 3
 __ver_sub__ = ""
 __ver_tuple__ = (__ver_major__,__ver_minor__,__ver_patch__,__ver_sub__)
 __version__ = "%d.%d.%d%s" % __ver_tuple__
